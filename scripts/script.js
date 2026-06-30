f9a95579bc40a9d9441bb494c09b7f86375f9789
Dat Tran <127409761+drxaviert@users.noreply.github.com>
2026-06-30 17:59:47 -0400

    refact: Update operator logic

--- scripts/script.js
+++ scripts/script.js
@@ -23,50 +23,59 @@ display.tabIndex = -1;
 display.value = 0;
 
 function operate(operatorArg, operandLArg, operandRArg) {
-  return operatorArg(operandLArg, operandRArg);
+  return operatorArg(+operandLArg, +operandRArg);
 }
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) =>
   button.addEventListener("click", (event) => {
-    let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let operator = ["+", "-", "*", "/"];
+    let numChecker = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let operatorChecker = ["+", "-", "*", "/"];
 
-    if (num.includes(+button.textContent)) {
-      if (operandL == undefined) {
-        operandL = +button.textContent;
+    if (numChecker.includes(+button.textContent)) {
+      if (operator == undefined) {
+        if (operandL == undefined) {
+          operandL = button.textContent;
+        } else {
+          operandL += button.textContent;
+        }
       } else {
-        operandR = +button.textContent;
+        if (operandR == undefined) {
+          operandR = button.textContent;
+        } else {
+          operandR += button.textContent;
+        }
       }
-      console.log("number");
+
+      // console.log("number");
     }
 
-    if (operator.includes(button.textContent)) {
+    if (operatorChecker.includes(button.textContent)) {
       operator = button.textContent;
 
-      if ((operator = "+")) {
+      if (operator == "+") {
         operatorFunc = add;
-      } else if ((operator = "-")) {
+      } else if (operator == "-") {
         operatorFunc = subtract;
-      } else if ((operator = "*")) {
+      } else if (operator == "*") {
         operatorFunc = multiply;
       } else {
         operatorFunc = divide;
       }
-
-      console.log(operatorFunc);
+      console.log(operator);
+      // console.log(operatorFunc);
     }
 
     if (button.textContent == "=") {
       result = operate(operatorFunc, operandL, operandR);
       console.log(result);
-      console.log("execute");
+      // console.log("execute");
     }
 
     if (button.textContent == "Clear") {
       console.log("clear");
     }
-    console.log(button.textContent);
-    display.value = button.textContent;
+    console.log([button.textContent, operandL, operandR]);
+    // display.value = button.textContent;
   }),
 );
