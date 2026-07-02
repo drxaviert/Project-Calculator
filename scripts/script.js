b019505d4759f8b1b3d3339e6b91a51791be5d2f
Dat Tran <127409761+drxaviert@users.noreply.github.com>
2026-07-02 16:53:31 -0400

    feat: Add keyboard support

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 let operatorFunc, operator, operandL, operandR, result;
+let numChecker = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+let operatorChecker = ["+", "-", "*", "/"];
 
 function add(a, b) {
   return a + b;
@@ -44,12 +46,110 @@ function operate(operatorArg, operandLArg, operandRArg) {
   return operatorArg(+operandLArg, +operandRArg);
 }
 
+// Keyboard logic
+const body = document.querySelector("body");
+body.addEventListener("keydown", (event) => {
+  if (numChecker.includes(event.key)) {
+    if (result != null) {
+      reset();
+    }
+    if (operator == null) {
+      if (operandL == null) {
+        if (event.key != ".") {
+          operandL = event.key;
+          display.value = operandL;
+        }
+      } else {
+        if (event.key != ".") {
+          operandL += event.key;
+          display.value = operandL;
+        } else {
+          if (!operandL.includes(".")) {
+            operandL += event.key;
+            display.value = operandL;
+          }
+        }
+      }
+    } else {
+      if (operandR == null) {
+        if (event.key != ".") {
+          if (event.key != ".") {
+            operandR = event.key;
+            display.value = operandR;
+          }
+        }
+      } else {
+        if (event.key != ".") {
+          operandR += event.key;
+          display.value = operandR;
+        } else {
+          if (!operandR.includes(".")) {
+            operandR += event.key;
+            display.value = operandR;
+          }
+        }
+      }
+    }
+
+    // console.log("number");
+  }
+
+  if (operatorChecker.includes(event.key)) {
+    if (operator != null && operandL != null && operandR != null) {
+      getResult();
+
+      operandL = result;
+      result = null;
+      operandR = null;
+    }
+
+    operator = event.key;
+
+    if (operator == "+") {
+      operatorFunc = add;
+    } else if (operator == "-") {
+      operatorFunc = subtract;
+    } else if (operator == "*") {
+      operatorFunc = multiply;
+    } else {
+      operatorFunc = divide;
+    }
+    console.log(operator);
+    // console.log(operatorFunc);
+  }
+
+  if (event.key == "Enter") {
+    if (operator != null && operandL != null && operandR != null) {
+      getResult();
+    } else {
+      reset();
+    }
+  }
+
+  if (event.key == "Backspace" && result == null) {
+    if (operator == null) {
+      operandL = operandL.slice(0, -1);
+      if (operandL == "") {
+        operandL = null;
+      }
+      display.value = operandL;
+    } else {
+      operandR = operandR.slice(0, -1);
+      if (operandR == "") {
+        operandR = null;
+      }
+
+      display.value = operandR;
+    }
+  }
+
+  console.log([event.key, operandL, operator, operandR, result]);
+});
+
+//Buttons logic
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) =>
   button.addEventListener("click", (event) => {
-    let numChecker = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-    let operatorChecker = ["+", "-", "*", "/"];
-
     if (numChecker.includes(button.textContent)) {
       if (result != null) {
         reset();
@@ -133,7 +233,7 @@ buttons.forEach((button) =>
       // console.log("clear");
     }
 
-    if (button.textContent == "<") {
+    if (button.textContent == "<" && result == null) {
       if (operator == null) {
         operandL = operandL.slice(0, -1);
         if (operandL == "") {
@@ -150,5 +250,6 @@ buttons.forEach((button) =>
       }
     }
     console.log([button.textContent, operandL, operator, operandR, result]);
+    button.blur();
   }),
 );
